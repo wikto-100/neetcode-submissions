26635ec4884a9d148f02943e6e20cce8a8ca1b67
wikto-100 <116840695+wikto-100@users.noreply.github.com>
2026-05-21 05:03:54 +0200

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+
+
+        unordered_map<int,int> fc;
+        vector<int> res(k);
+        for(int num : nums)
+            fc[num]++;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> heap;
+        for(const auto &[v,f] : fc){
+            heap.push({f,v});
+            if(heap.size() > k)
+                heap.pop();
+        }
+        for(int i = 0; i < k; i++){
+            res[i] = heap.top().second;
+            heap.pop();
+        }
+        return res;
+    }
+};
