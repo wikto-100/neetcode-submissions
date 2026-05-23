7ffbe8071e645df0b87e622c93670cbb84c95c1e
wikto-100 <116840695+wikto-100@users.noreply.github.com>
2026-05-23 06:01:59 +0200

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.length();
+        bool odd = (n % 2) != 0;
+        vector<char> clear;
+        for(int i = 0; i < n; i++){
+            if(isalnum(s[i])) clear.emplace_back(tolower(s[i]));
+        }
+        for(int i = 0,j=clear.size()-1; i < j; i++,j--){
+            if(clear[i] != clear[j]) {return false;}
+
+        }
+        return true;
+    }
+};
