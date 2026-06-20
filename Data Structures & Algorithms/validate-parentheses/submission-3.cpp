df9993f2006638632902ffe5c83ff1cc011807fc
wikto-100 <116840695+wikto-100@users.noreply.github.com>
2026-06-20 13:22:55 +0200

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,32 @@
+class Solution {
+   public:
+    bool isValid(string s) {
+        stack<char> par;
+
+        for (const auto& c : s) {
+            if (c == '(' || c == '[' || c == '{')
+                par.push(c);
+            
+            if(par.empty()) return false;
+
+            if (c == ')') {
+                if (par.top() == '(')
+                    par.pop();
+                else
+                    return false;
+            } else if (c == ']') {
+                if (par.top() == '[')
+                    par.pop();
+                else
+                    return false;
+            } else if (c == '}') {
+                if (par.top() == '{')
+                    par.pop();
+                else
+                    return false;
+            }
+        }
+
+        return par.empty();
+    }
+};
