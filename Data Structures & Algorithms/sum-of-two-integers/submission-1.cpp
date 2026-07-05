18c007f3a0f5fee3829ee905d22b274d7b5bbc45
wikto-100 <116840695+wikto-100@users.noreply.github.com>
2026-07-05 18:42:40 +0200

    Add: sum-of-two-integers - submission-1

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        bool sgna = a & (1u << 31);
+        bool sgnb = b & (1u << 31);
+        unsigned bit = 1u, carry= 0u;
+        unsigned result = 0;
+        do{
+            result |= (a^b^carry)&bit; // 1 or 3
+            carry = (((a&b)|(a&carry)|(b&carry)) & bit) << 1; // any 2 or 3
+
+            printf("a_bits=%08x b_bits=%08x result=%08x\n", a & bit, b & bit, result);
+
+        }while((bit = bit << 1) < (1u << 31));
+            result |= (a^b^carry)&bit; // 1 or 3
+            carry = (((a&b)|(a&carry)|(b&carry)) & bit) << 1; // any 2 or 3
+        return result;
+    }
+};
